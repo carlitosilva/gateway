47d1777a6244f25b0f8124256b065436a571d8a0
carlitosilva <carlito.silva@gmail.com>
2024-07-05 21:28:59 -0300

    Correção

--- src/main/java/br/jus/tjba/api/push/gateway/GatewayApplication.java
+++ src/main/java/br/jus/tjba/api/push/gateway/GatewayApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class GetewayApplication {
+public class GatewayApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(GetewayApplication.class, args);
+		SpringApplication.run(GatewayApplication.class, args);
 	}
 
 }
